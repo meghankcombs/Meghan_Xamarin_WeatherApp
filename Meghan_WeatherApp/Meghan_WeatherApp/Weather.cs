ef7cae0d10db5a13d1b9e3a96e4bf7761a7b88a6
Meghan <meghankcombs@gmail.com>
2018-04-23 15:15:20 -0400

    Initial Git Extension commit

---
+++ Meghan_WeatherApp/Meghan_WeatherApp/Weather.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Meghan_WeatherApp
+{
+    public class Weather
+    {
+        public string Title { get; set; }
+        public string Temperature { get; set; }
+        public string Wind { get; set; }
+        public string Humidity { get; set; }
+        public string Visibility { get; set; }
+        public string Sunrise { get; set; }
+        public string Sunset { get; set; }
+
+        public Weather()
+        {
+            //Labels are bound to properties, so set to empty strings so labels show up on all platforms
+            this.Title = "";
+            this.Temperature = "";
+            this.Wind = "";
+            this.Humidity = "";
+            this.Visibility = "";
+            this.Sunrise = "";
+            this.Sunset = "";
+        }
+    }
+}
---
+++ Meghan_WeatherApp/Meghan_WeatherApp/WeatherPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Meghan_WeatherApp
+{
+	//[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class WeatherPage : ContentPage
+	{
+		public WeatherPage()
+		{
+			InitializeComponent();
+            this.Title = "Meghan's Weather App";
+            getWeatherBtn.Clicked += GetWeatherBtn_Clicked;
+
+            //default binding set to default object
+            this.BindingContext = new Weather();
+		}
+
+        private async void GetWeatherBtn_Clicked(object sender, EventArgs e)
+        {
+            Weather weather = await Core.GetWeather("60601");
+            getWeatherBtn.Text = weather.Title;
+        }
+	}
+}
